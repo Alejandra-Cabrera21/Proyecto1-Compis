4b3433a9da99dc8334f4ff1b5941f3e4f8888678
Alejandra-Cabrera21 <124795117+Alejandra-Cabrera21@users.noreply.github.com>
2025-05-18 14:12:21 -0600

    Update App.java

--- demo/src/main/java/proyecto/App.java
+++ demo/src/main/java/proyecto/App.java
@@ -1,13 +1,30 @@
 package proyecto;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+import proyecto.antlr.ExprLexer;
+import proyecto.antlr.ExprParser;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            CharStream input = CharStreams.fromFileName("prueba1.txt");
+
+            ExprLexer lexer = new ExprLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            ExprParser parser = new ExprParser(tokens);
+
+            ParseTree tree = parser.programa(); // la regla inicial se llama programa
+
+            PythonGeneratorVisitor visitor = new PythonGeneratorVisitor();
+            visitor.visit(tree);
+            visitor.close();
+
+
+            System.out.println("Traducción a Python generada en el archivo salida.py");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
